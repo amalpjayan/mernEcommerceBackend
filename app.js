54b4e171acb6c562cc65524c91c78f797c1fffcd
amalpjayan <amalpjayan527@gmail.com>
2023-03-05 07:43:48 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -31,11 +31,11 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname, "../build")));
+// app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../build/index.html"));
+// });
 
 //Middlewares for errors
 app.use(errorMiddleware);
